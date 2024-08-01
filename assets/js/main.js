9f15f90c1be2775f3ad7605c7b6f460b33ff4af4
XayrulloWeb <xayrulloweb@gmail.com>
2024-08-01 16:15:31 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+/*=============== EXPANDED LIST ===============*/
+const navExpand = document.getElementById('nav-expand'),
+      navExpandList = document.getElementById('nav-expand-list'),
+      navExpandIcon = document.getElementById('nav-expand-icon')
+
+navExpand.addEventListener('click', () => {
+   // Expand list
+   navExpandList.classList.toggle('show-list')
+
+   // Rotate icon
+   navExpandIcon.classList.toggle('rotate-icon')
+})
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+
+// POPUP
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+let closePopupHome = document.querySelector('.orders_start');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === closePopupHome) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
